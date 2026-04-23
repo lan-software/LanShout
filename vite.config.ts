d3bee7d35358df501269d17d2f479362c8b85df6
Markus Kohn <post@markus-kohn.com>
2026-04-23 13:21:45 +0200

    Update Vite server configuration to use environment variable for port and enable HMR

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,11 @@ export default defineConfig({
         }),
     ],
     server: {
-        port: 5172,
+        host: '0.0.0.0',
+        port: Number(process.env.VITE_PORT) || 5175,
         strictPort: true,
+        hmr: {
+            host: 'localhost',
+        },
     },
 });
